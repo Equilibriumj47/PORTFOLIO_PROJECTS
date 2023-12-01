7ee73405acde6fa5e9a47ff4ed98c4fd75dbf77a
Equilibriumj47 <127885495+Equilibriumj47@users.noreply.github.com>
2023-12-01 08:20:58 -0500

    Add files via upload
    Google Analytics Capstone Project: Cyclistic Bike Share Company

---
+++ CYCLISTIC BIKESHARE GOOGLE DATA ANALYTICS.R
@@ -0,0 +1,166 @@
+### Google Data Analytics Capstone Project  ###
+
+# This analysis is based on Cyclistic Bike Share Company written by Germane Adeyanju. The purpose of this script is to consolidate the 12 months downloaded Bikeshare company's data into a single dataframe and then conduct simple analysis to help answer the key question: “In what ways do members and casual riders use Cyclistic bikes differently?”
+
+# # # # # # # # # # # # # # # # # # # # # # # 
+# Install required packages
+# tidyverse for data import and wrangling
+# lubridate for date functions
+# ggplot for visualization
+# # # # # # # # # # # # # # # # # # # # # # #  
+
+library(tidyverse)  #helps wrangle data
+library(lubridate)  #helps wrangle date attributes
+library(ggplot2)  #helps visualize data
+getwd() #displays your working directory
+setwd("‪C:\Users\RBS Admin\Desktop\CAPSTONE_CYCLISTIC_DATA") #sets your working directory to simplify calls to data ... make sure to use your OWN username instead of mine ;)
+
+#=====================
+# STEP 1: COLLECT DATA
+#=====================
+D1<-read.csv(file.choose())
+D2 <-read.csv(file.choose())
+D3 <- read.csv(file.choose())
+D4 <- read.csv(file.choose())
+D5 <- read.csv(file.choose())
+D6 <- read.csv(file.choose())
+D7 <-read.csv("C://Users//RBS Admin//Desktop//CAPSTONE_CYCLISTIC_DATA//202208-divvy-tripdata.csv")
+D8 <- read.csv("C://Users//RBS Admin//Desktop//CAPSTONE_CYCLISTIC_DATA//202209-divvy-publictripdata.csv")
+D9 <- read.csv("C://Users//RBS Admin//Desktop//CAPSTONE_CYCLISTIC_DATA//202210-divvy-tripdata.csv")
+D10 <- read.csv("C://Users//RBS Admin//Desktop//CAPSTONE_CYCLISTIC_DATA//202211-divvy-tripdata.csv")
+D11<- read.csv("C://Users//RBS Admin//Desktop//CAPSTONE_CYCLISTIC_DATA//202212-divvy-tripdata.csv")
+D12 <- read.csv("C://Users//RBS Admin//Desktop//CAPSTONE_CYCLISTIC_DATA//202301-divvy-tripdata.csv")
+#====================================================
+# STEP 2: WRANGLE DATA AND COMBINE INTO A SINGLE FILE
+#====================================================
+# Compare column names each of the files
+# While the names don't have to be in the same order, they DO need to match perfectly before we can use a command to join them into one file
+colnames(D1)
+colnames(D2)
+colnames(D3)
+colnames(D4)
+colnames(D5)
+colnames(D6)
+colnames(D7)
+colnames(D8)
+colnames(D9)
+colnames(D10)
+colnames(D11)
+colnames(D12)
+# Rename columns  to make them consistent with D1
+
+#--------------------------------------------------------------------#
+# Stack individual data frame into quarterly
+Q1_trips <- bind_rows(D1,D2,D3)
+Q2_trips <- bind_rows(D4,D5,D6)
+Q3_trips <- bind_rows(D7,D8,D9)
+Q4_trips <- bind_rows(D10,D11,D12)
+# Stack individual quarter's data frames into one big data frame
+all_trips <- bind_rows(Q1_trips, Q2_trips, Q3_trips, Q4_trips)
+
+#======================================================
+# STEP 3: CLEAN UP AND ADD DATA TO PREPARE FOR ANALYSIS
+#======================================================
+# Inspect the new table that has been created
+colnames(all_trips)  #List of column names
+nrow(all_trips)  #How many rows are in data frame?
+dim(all_trips)  #Dimensions of the data frame?
+head(all_trips)  #See the first 6 rows of data frame.  Also tail(all_trips)
+str(all_trips)  #See list of columns and data types (numeric, character, etc)
+summary(all_trips)  #Statistical summary of data. Mainly for numerics
+
+#---------------------------------------------------------------
+# Add columns that list the date, month, day, and year of each ride
+# This will allow us to aggregate ride data for each month, day, or year ... before completing these operations we could only aggregate at the ride level
+
+all_trips$date <- as.Date(all_trips$started_at) #The default format is yyyy-mm-dd
+all_trips$month <- format(as.Date(all_trips$date), "%m")
+all_trips$day <- format(as.Date(all_trips$date), "%d")
+all_trips$year <- format(as.Date(all_trips$date), "%Y")
+all_trips$day_of_week <- format(as.Date(all_trips$date), "%A")
+
+# Add a "ride_length" calculation to all_trips (in seconds)
+
+all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at)
+
+# Inspect the structure of the columns
+str(all_trips)
+#===================================================================
+# Convert "ride_length" from Factor to numeric so we can run calculations on the data
+is.factor(all_trips$ride_length)
+all_trips$ride_length <- as.numeric(as.character(all_trips$ride_length))
+is.numeric(all_trips$ride_length)
+
+# Remove "bad" data
+# The dataframe includes a few hundred entries when bikes were taken out of docks and checked for quality by Cyclistic Bikeshare or ride_length was negative
+# We will create a new version of the dataframe (v2) since data is being removed
+
+all_trips_v2 <- all_trips[!(all_trips$start_station_name == "HQ QR" | all_trips$ride_length<0),]
+
+# STEP 4: CONDUCT DESCRIPTIVE ANALYSIS
+#=====================================
+# Descriptive analysis on ride_length (all figures in seconds)
+mean(all_trips_v2$ride_length) #straight average (total ride length / rides)
+median(all_trips_v2$ride_length) #midpoint number in the ascending array of ride lengths
+max(all_trips_v2$ride_length) #longest ride
+min(all_trips_v2$ride_length) #shortest ride
+
+# condense the four lines above to one line using summary() on the specific attribute
+summary(all_trips_v2$ride_length)
+
+# Compare members and casual users
+aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = mean)
+aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = median)
+aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = max)
+aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = min)
+
+# See the average ride time by each day for members vs casual users
+aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+
+# Fixing the days of the week that are out of order.
+all_trips_v2$day_of_week <- ordered(all_trips_v2$day_of_week, levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
+
+#==================================================================================
+# Run the average ride time by each day for members vs casual users
+aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+
+# analyze ridership data by type and weekday
+all_trips_v2 %>% 
+  mutate(weekday = wday(started_at, label = TRUE)) %>%  #creates weekday field using wday()
+  group_by(member_casual, weekday) %>%  #groups by usertype and weekday
+  summarise(number_of_rides = n()							#calculates the number of rides and average duration 
+            ,average_duration = mean(ride_length)) %>% 		# calculates the average duration
+  arrange(member_casual, weekday)								# sorts
+
+# Visualize the number of rides by rider type
+all_trips_v2 %>% 
+  mutate(weekday = wday(started_at, label = TRUE)) %>% 
+  group_by(member_casual, weekday) %>% 
+  summarise(number_of_rides = n()
+            ,average_duration = mean(ride_length)) %>% 
+  arrange(member_casual, weekday)  %>% 
+  ggplot(aes(x = weekday, y = number_of_rides, fill = member_casual)) +
+  geom_col(position = "dodge") + labs(title="Cyclistic Bikeshare: Number of Rides Vs. Weekday")
+
+
+# Let's create a visualization for average duration
+all_trips_v2 %>% 
+  mutate(weekday = wday(started_at, label = TRUE)) %>% 
+  group_by(member_casual, weekday) %>% 
+  summarise(number_of_rides = n()
+            ,average_duration = mean(ride_length)) %>% 
+  arrange(member_casual, weekday)  %>% 
+  ggplot(aes(x = weekday, y = average_duration, fill = member_casual)) +
+  geom_col(position = "dodge") + labs(title="Cyclistic Bikeshare: Average Duration Vs. Weekday")
+
+#===========================================================================
+
+#=================================================
+# STEP 5: EXPORT SUMMARY FILE FOR FURTHER ANALYSIS
+#=================================================
+# Create a csv file that we will visualize in Excel, Tableau, or my presentation software
+
+counts <- aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+write.csv(counts, file = 'C://Users//RBS Admin//Desktop//CAPSTONE_CYCLISTIC_DATA//avg_ride_length.csv')
+
+# done!
